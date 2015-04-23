eb95d7626dfb937dd2f3f46f422722623d1ecc52
Guido Faassen <guidof_1991@hotmail.com>
2015-04-23 16:46:13 +0200

    .

--- test_myFunction.py
+++ test_myFunction.py
@@ -2,3 +2,9 @@ from myFunction import add_one
 
 def test_answer():
     assert add_one(3) == 4
+
+def test_answer2():
+    assert add_one(4) == 5
+
+def test_answer3():
+    assert add_one(-2) == -1
