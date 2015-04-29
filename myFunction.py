8d715a2607fc3a483dcb4e112c74431fde8aae16
Guido Faassen <guidof_1991@hotmail.com>
2015-04-29 16:31:21 +0200

    Added some tests

--- myFunction.py
+++ myFunction.py
@@ -1,5 +1,24 @@
+import numpy as np
+
 def add_one(x):
 	return x+1
 
-if __name__ == '__main__':
-    print("Hello %s" % (some_function()))
+def similarity_conditional(freqUV,freqU, u, v):
+    # log= logging.getLogger( "SomeTest.testSomething" )
+    # log.debug( "this= %r", freqU[u])
+    if (freqU[u] == 0) or (freqU[v] == 0):
+        print("WARNING: Cannot calculate conditional similarity (divide by zero?). Return 0")
+        return 0
+    else:
+        return freqUV[u,v] / (freqU[u] * freqU[v])
+
+
+def myDivide(a,b):
+	try:
+		return a/b
+	except:
+		return 0
+# print(myDivide(1,0))
+
+# if __name__ == '__main__':
+#     print("Hello %s" % (some_function()))
--- test_myFunction.py
+++ test_myFunction.py
@@ -1,10 +1,36 @@
-from myFunction import add_one
+import myFunction
+import numpy as np
 
-def test_answer():
-    assert add_one(3) == 4
+class TestClass:
+	def test_answer(self):
+	    assert myFunction.add_one(3) == 4
 
-def test_answer2():
-    assert add_one(4) == 5
+	def test_answer2(self):
+	    assert myFunction.add_one(4) == 5
 
-def test_answer3():
-    assert add_one(-2) == -1
+	def test_answer3(self):
+	    assert myFunction.add_one(-2) == -1
+
+	# def test_similarity_conditional2(self):
+	# 	freqUV = np.asarray([[2,3],[3,4]])
+	# 	freqU = np.asarray([2,0])
+	# 	u = 0
+	# 	v = 1
+	# 	assert myFunction.similarity_conditional(freqUV,freqU, u, v) == 0
+
+	def test_similarity_conditional(self):
+		freqUV = np.asarray([[2,3],[3,4]])
+		freqU = np.asarray([2,0])
+		u = 0
+		v = 1
+		assert myFunction.similarity_conditional(freqUV,freqU, u, v) == 0
+
+	def test_similarity_conditional2(self):
+		freqUV = np.asarray([[2,3],[3,4]])
+		freqU = np.asarray([2,1])
+		u = 0
+		v = 1
+		assert myFunction.similarity_conditional(freqUV,freqU, u, v) == 1.5
+
+	def test_myDivide(self):
+		assert myFunction.myDivide(1,0) == 0
